f09e0c965b05dff5651d772e7e138760e864baa9
Patradanai Nakpimay <patradanai.na@tnds.com>
2023-07-21 02:25:46 +0700

    fix: page

--- .plop/plopfile.js
+++ .plop/plopfile.js
@@ -1,10 +1,12 @@
 const component = require('./components/config')
 const moduleComponent = require('./modules/config')
 const moduleStore = require('./stores/config')
+const modulePage = require('./pages/config')
 
 // Add here more generators
 module.exports = function (plop) {
     plop.setGenerator('component', component)
     plop.setGenerator('module', moduleComponent)
     plop.setGenerator('store', moduleStore)
+    plop.setGenerator('page', modulePage)
 }
