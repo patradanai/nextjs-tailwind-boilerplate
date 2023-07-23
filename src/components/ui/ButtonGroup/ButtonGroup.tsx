4167e7bb7cc06a71dd050d8b095d30ab6e94c71d
Patradanai Nakpimay <patradanai.na@tnds.com>
2023-07-24 02:19:13 +0700

    feat: init btn group

---
+++ src/components/ui/ButtonGroup/ButtonGroup.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { NextPage } from 'next'
+
+/**
+ *
+ * ButtonGroup
+ *
+ */
+
+interface Props {}
+
+const ButtonGroup: NextPage<Props> = () => {
+  return (
+    <div>
+      ButtonGroup
+    </div>
+  )
+}
+ 
+ 
+export default ButtonGroup
